9197188fc66e7119dfa9350da93b0bdef2d0a7b7
--help <sayeedahmed.1310@gmail.com>
2020-05-31 19:40:38 -0400

    initial commit

---
+++ src/views/Dogs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <PetTable
+      species="dogs"
+      v-bind:pets="dogs"
+    />
+  </div>
+</template>
+
+<script>
+// import dogs from '@/data/dogs'
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+
+export default {
+  components: {
+    PetTable
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Pet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ $route.params.id }}
+    <h1>{{ currentAnimal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ currentAnimal.age }} years old</p>
+    <p>Breed: {{ currentAnimal.breed }}</p>
+  </div>
+</template>
+
+<script>
+// import cats from '@/data/cats'
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      currentAnimal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.currentAnimal = animal
+  }
+}
+</script>
